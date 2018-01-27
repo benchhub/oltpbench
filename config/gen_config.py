4d0388f9f7b1419c75623e69c47d8d897ab280a6
at15 <at15@outlook.com>
2018-01-27 00:08:35 -0800

    config: Finish catalog for benchmarks

--- config/gen_config.py
+++ config/gen_config.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+generate config file based on sample config
+TODO: might change it to config gen, config benchmark etc.
+"""
 
 import argparse
 import yaml
@@ -51,16 +55,23 @@ supported_databases = {
     'memsql'
 }
 
-# generate config file based on sample config
-# def help():
+
+def valid_benchmarks():
+    with open('benchmarks.yml', 'r') as f:
+        try:
+            yaml.load(f)
+        except yaml.YAMLError as ex:
+            print(ex)
 
 def main():
-    parser = argparse.ArgumentParser(description='generate config based on sample config')
-    parser.add_argument('benchmark', metavar='benchmark', type=str, choices=supported_benchmarks, help='benchmark type i.e. tpcc, tpch')
-    parser.add_argument('system', metavar='system', type=str, choices=supported_databases, help='target database system i.e. mysql, postgres')
-    args = parser.parse_args()
-    benchmark = args.benchmark
-    system = args.system
+    # TODO: sub command? load choices from YAML file?
+    # parser = argparse.ArgumentParser(description='generate config based on sample config')
+    # parser.add_argument('benchmark', metavar='benchmark', type=str, choices=supported_benchmarks, help='benchmark type i.e. tpcc, tpch')
+    # parser.add_argument('system', metavar='system', type=str, choices=supported_databases, help='target database system i.e. mysql, postgres')
+    # args = parser.parse_args()
+    # benchmark = args.benchmark
+    # system = args.system
+    valid_benchmarks()
 
 
 if __name__ == '__main__':
